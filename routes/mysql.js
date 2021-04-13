fcd811ad1188f4ecc9848bc5be6a039d16d54339
Kelo <1376312818@qq.com>
2021-04-13 18:55:38 +0800

    add lib mysql

---
+++ routes/mysql.js
@@ -0,0 +1,51 @@
+const mysql = require("mysql");
+const fs = require("fs");
+const path = require("path");
+
+const mysqlConfigPath = path.join(__dirname, "mysql.json");
+const mysqlConfig = JSON.parse(fs.readFileSync(mysqlConfigPath));
+
+const pool = mysql.createPool(mysqlConfig);
+
+class Exit {
+  constructor() {
+    // 是否已终止与数据库的连接
+    this.exec = false;
+
+
+    // windows 特判
+    if (process.platform === "win32") {
+      var rl = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+      rl.on("SIGINT", function () {
+        process.emit("SIGINT");
+      });
+    }
+
+    process.on("SIGINT", () => {
+      this.end(() => {
+        process.exit();
+      });
+    });
+    process.on("beforeExit", () => {
+      this.end(() => {
+        process.exit();
+      });
+    });
+  }
+  end(cb) {
+    if (this.exex) return;
+    pool.end(() => {
+      console.log("SIGINT: POOL END CONNECTION");
+      this.exex = true;
+      if (typeof (cb) == "function") cb();
+    });
+  }
+}
+
+let exit = new Exit();
+
+module.exports = pool;
