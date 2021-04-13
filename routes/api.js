07acfcb1a3618070af20cf50b43bdf88fab04c19
John_Wilson <727706242@qq.com>
2021-04-13 21:50:32 +0800

    代码风格修复

--- routes/api.js
+++ routes/api.js
@@ -27,21 +27,21 @@ router.get('/seats/query', (req, res, next) => {
   });
 });
 
-//查询学生信息
+// 查询学生信息
 router.get('/students/query', (req, res, next) => {
   pool.query('SELECT Id,Name,Major,Credit FROM Student WHERE ?', req.query, (error, results, fields) => {
     res.json(handleSQLResult(error, results, fields));
   });
 });
 
-//修改学生的信誉积分
+// 修改学生的信誉积分
 router.post('/students/quary', (req, res, next) => {
   pool.query('UPDATE Student Set Credit=Credit-1  WHERE ?', req.query.Id, (error, results, fields) => {
     res.json(handleSQLResult(error, results, fields));
   });
 });
 
-/*/增加一条座位预约
+/* /增加一条座位预约
 router.get('/students/query', (req, res, next) => {
   pool.query('UPDATE Student Set Credit=Credit-1  WHERE ?', req.query, (error, results, fields) => {
     res.json(handleSQLResult(error, results, fields));
