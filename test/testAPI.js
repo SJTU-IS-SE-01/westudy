a0c0ec12ded69d853b4a530a8baf2d6cd846b9b8
Kelo <1376312818@qq.com>
2021-05-17 19:40:52 +0800

    use mocha to test

--- test/testAPI.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.post('http://localhost:3000/api/students/quary?Id=001')
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
