a03381f29a8e4e205a014250228b8a407f6bad67
Kelo <1376312818@qq.com>
2021-05-17 23:06:21 +0800

    添加注册功能

---
+++ public/javascript/signup.js
@@ -0,0 +1,35 @@
+const { $ } = window;
+
+function checkValid() {
+  if (!/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test($('#inputEmail').val())) {
+    alert('请输入正确的邮箱');
+    return false;
+  }
+  return true;
+}
+
+$(document).ready(() => {
+  $('#get-code').click(() => {
+    if (!checkValid()) return;
+    // console.log("je")
+    $.post('/code', { email: $('#inputEmail').val() }, (data) => {
+      console.log(data);
+      alert('发送成功');
+    });
+  });
+  $('#signup').click((e) => {
+    e.preventDefault();
+    const email = $('#inputEmail').val();
+    const code = $('#inputCode').val();
+    const Id = $('#inputSno').val();
+    const Name = $('#inputName').val();
+    const Major = $('#inputSdept').val();
+    $.post('/signup', {
+      email, code, Id, Name, Major,
+    }, (data) => {
+      if (data.status === 0) {
+        window.location.href = '/';
+      }
+    });
+  });
+});
