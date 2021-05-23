7d2823562abf8e7c47cd915227312e6e5a619717
leishilin <80253860+leishilin@users.noreply.github.com>
2021-05-23 17:34:57 +0800

    Add files via upload

---
+++ public/javascript/index.js
@@ -0,0 +1,36 @@
+const { $ } = window;
+class Header {
+  build() {
+    const header = $('#header');
+    if (header.length === 0) return;
+    $.get('/header.html', (data) => {
+      header.html(data);
+      header.trigger('build');
+    });
+    header.bind('build', this.update.bind(this));
+  }
+
+  update() {
+    $('#login').click((e) => {
+      e.preventDefault();
+      window.location.href = '/login.html';
+    });
+    $('#signup').click((e) => {
+      e.preventDefault();
+      window.location.href = '/signup.html';
+    });
+    $.get('/users/getEmail', (data) => {
+      console.log(data);
+      if (data.status === 0) {
+        $('#login').hide();
+        $('#signup').hide();
+        $('#users-email').text(data.results.email);
+      }
+    });
+  }
+}
+
+$(document).ready(() => {
+  const header = new Header();
+  header.build();
+});
---
+++ public/javascript/signup.js
@@ -0,0 +1,37 @@
+const { $, toastr } = window;
+
+function checkValid() {
+  if (!/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test($('#inputEmail').val())) {
+    alert('请输入正确的邮箱');
+    return false;
+  }
+  return true;
+}
+
+$(document).ready(() => {
+  $('#get-code').click(() => {
+    if (!checkValid()) return;
+    $.post('/users/code', { email: $('#inputEmail').val() }, (data) => {
+      console.log(data);
+      // alert('发送成功');
+    });
+  });
+  $('#signup').click((e) => {
+    e.preventDefault();
+    const email = $('#inputEmail').val();
+    const code = $('#inputCode').val();
+    const Id = $('#inputSno').val();
+    const Name = $('#inputName').val();
+    const Major = $('#inputSdept').val();
+    $.post('/users/signup', {
+      email, code, Id, Name, Major,
+    }, (data) => {
+      if (data.status === 0) {
+        window.location.href = '/';
+      } else {
+        toastr.options.positionClass = 'toast-top-center';
+        toastr.error(data.results);
+      }
+    });
+  });
+});
