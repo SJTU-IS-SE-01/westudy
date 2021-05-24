5cf47628993159e7020b8dd310b1732e2e4be116
Qnzzzz <qnzzzz@sjtu.edu.cn>
2021-05-24 19:59:32 +0800

    personal.js v2

--- public/javascript/personal.js
+++ public/javascript/personal.js
@@ -1,15 +1,16 @@
-$(document).ready(() => {
-    $.get('/users/getEmail',(data1) =>{
-        console.log(data1);
-        const email = data1.results.email;
-        $.get('/api/students/query?email=' + email,(data2) =>{
-            console.log(data2);
-            $('#name').html(data2.results[0].Name);
-            $('#id').html(data2.results[0].Id);
-            $('#major').html(data2.results[0].Major);
-            $('#credit').html(data2.results[0].Credit);
-        })
-    })
-    
+window.$(document).ready(() => {
+  const {
+    $,
+  } = window;
+  $.get('/users/getEmail', (data1) => {
+    console.log(data1);
+    const { email } = data1.results;
+    $.get(`/api/students/query?email=${email}`, (data2) => {
+      console.log(data2);
+      $('#name').html(data2.results[0].Name);
+      $('#id').html(data2.results[0].Id);
+      $('#major').html(data2.results[0].Major);
+      $('#credit').html(data2.results[0].Credit);
+    });
   });
-  
+});
