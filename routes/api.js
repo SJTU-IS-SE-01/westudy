f620ae8a2dc8a69fc305ad87d525c91a9736f663
John_Wilson <727706242@qq.com>
2021-05-24 19:28:11 +0800

    增加作为预约，查询某段时间空闲的座位编号

--- routes/api.js
+++ routes/api.js
@@ -27,8 +27,8 @@ router.get('/seats/query', (req, res, next) => {
   });
 });
 
-// 查询座位的状态,1表示被占，0表示未被占
-router.get('/seatcheck/:id/:Btime/:Etime', (req, res, next) => {
+// 查询某段时间某个座位的状态,1表示被占，0表示未被占
+router.get('/seatcheck/:Snum/:Btime/:Etime', (req, res, next) => {
   if (req.params.Etime < req.params.Btime) {
     res.json({
       status: 1,
@@ -36,7 +36,7 @@ router.get('/seatcheck/:id/:Btime/:Etime', (req, res, next) => {
       results: {},
     });
   } else {
-    pool.query('SELECT seatcheck FROM SeatStatus WHERE Snum=? AND unix_timestamp(Btime)<=unix_timestamp(?) AND unix_timestamp(Etime)>=unix_timestamp(?)   ', [req.params.id, req.params.Etime, req.params.Btime], (error, results, fields) => {
+    pool.query('SELECT seatcheck FROM SeatStatus WHERE Snum=? AND unix_timestamp(Btime)<=unix_timestamp(?) AND unix_timestamp(Etime)>=unix_timestamp(?)   ', [req.params.Snum, req.params.Etime, req.params.Btime], (error, results, fields) => {
       const json = handleSQLResult(error, results, fields);
       if (json.results.length === 0) {
         json.results = 0;
@@ -64,16 +64,32 @@ router.post('/students/quary', (req, res, next) => {
 
 // 增加一条座位预约
 router.post('/students/addappointment', (req, res, next) => {
-  pool.query('INSERT INTO SeatStatus(Btime,Etime,Snum,Id) values(?,?,?,?) ', req.query, (error, results, fields) => {
+  pool.query('INSERT INTO SeatStatus(Btime,Etime,Snum,Id) values(?,?,?,?) ',[req.query.Btime,req.query.Etime,req.query.Snum,req.query.Id],(error, results, fields) => {
     res.json(handleSQLResult(error, results, fields));
   });
 });
 
-// 查询座位预约信息 seatsapt=座位预约
+// 查询某个座位所有的预约信息 seatsapt=座位预约
 router.get('/students/seatsapt', (req, res, next) => {
   pool.query('SELECT Btime,Etime,Snum,Id from SeatStatus WHERE ?', req.query, (error, results, fields) => {
     res.json(handleSQLResult(error, results, fields));
   });
+});
+
+//查询某段时间内空闲的座位编号
+router.get('/timecheck/:Btime/:Etime', (req, res, next) => {
+  if (req.params.Etime < req.params.Btime) {
+    res.json({
+      status: 1,
+      msg: 'Btime>Etime',
+      results: {},
+    });
+  } else {
+    pool.query('SELECT Snum FROM Seat WHERE Snum NOT IN (SELECT Snum FROM SeatStatus WHERE  unix_timestamp(Btime)<=unix_timestamp(?) AND unix_timestamp(Etime)>=unix_timestamp(?))  ', [req.params.Etime, req.params.Btime], (error, results, fields) => {
+      const json = handleSQLResult(error, results, fields);
+      res.json(json);
+    });
+  }
 });
 
 module.exports = router;
