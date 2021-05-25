d1faa07bcff1a8ce819861e318abcf97c147f789
John_Wilson <727706242@qq.com>
2021-05-25 19:10:48 +0800

    关于api和测试文档改进

--- test/backend.test.js
+++ test/backend.test.js
@@ -99,6 +99,26 @@ describe('TEST API', () => {
         return done();
       });
   });
+
+  // 查询学生信息
+  //测试改进
+  // 更加便捷
+  it('/students/query', (done) => {
+    request
+      .get('/api/students/query?Id=519021911101')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.strictEqual(res.body.status, 0);
+        assert.strictEqual(res.body.msg, 'ok');
+        assert.strictEqual(res.body.results[0].Id, '519021911111');
+        assert.strictEqual(res.body.results[0].Name, 'John');
+        assert.strictEqual(res.body.results[0].Major, 'IS');
+        return done();
+      });
+  });
+  
   after((done) => {
     pool.exitHelper.end(done);
   });
