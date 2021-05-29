ef3798e34cdc471def71adeaa89a07a26cfa4e60
Kelo <1376312818@qq.com>
2021-05-29 20:30:57 +0800

    beautify html

--- public/javascript/appoint.js
+++ public/javascript/appoint.js
@@ -1,12 +1,12 @@
 function newtab(id) {
   if (id === '0') {
-    document.getElementById('0').className = 'active';
-    document.getElementById('1').className = '';
+    document.getElementById('0').className = 'active il-link';
+    document.getElementById('1').className = 'il-link';
     document.getElementById('nMfOyStPrXdWcNfH').style.display = 'block';
     document.getElementById('ePvFrNcCkWqOsRrW').style.display = 'none';
   } else {
-    document.getElementById('0').className = '';
-    document.getElementById('1').className = 'active';
+    document.getElementById('0').className = 'il-link';
+    document.getElementById('1').className = 'active il-link';
     document.getElementById('nMfOyStPrXdWcNfH').style.display = 'none';
     document.getElementById('ePvFrNcCkWqOsRrW').style.display = 'block';
   }
