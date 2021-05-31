2973f55d0e8e4344eaafbbf0ea736f43344ef68c
JohnWilson614 <73482427+JohnWilson614@users.noreply.github.com>
2021-05-31 00:36:37 -0700

    数据库文件更新 (#74)
    * 邮件内容修改
    
    * 数据库文件修改

---
+++ mysqlconfig/Student.sql
@@ -0,0 +1,118 @@
+create table Student(
+	Id varchar(20) PRIMARY KEY,
+	Name varchar(20),
+	Major varchar(20),
+	Credit int CHECK (Credit <= 100),
+    Email varchar(50),
+) DEFAULT CHARSET=utf8;
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910000', '安达街', 'CS', 100, '10000000@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910001', '十六块', 'CS', 100, '10000001@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910002', '宋佳', 'CS', 100, '10000002@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910003', '问恩', 'CS', 100, '10000003@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910004', '阿斯顿南方', 'CS', 100, '10000004@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910005', '欧舒丹', 'CS', 100, '10000005@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910006', '阿斯蒂芬', 'CS', 100, '10000006@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910007', '发大水', 'CS', 100, '10000007@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910008', '暗示', 'CS', 100, '10000008@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910009', '魏然', 'CS', 100, '10000009@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910010', '施工图', 'CS', 100, '10000010@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910011', '薯条', 'CS', 100, '10000011@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910012', '使用', 'CS', 100, '10000012@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910013', '所有人', 'CS', 100, '10000013@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910014', '树獭', 'CS', 100, '10000014@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910015', '老虎', 'CS', 100, '10000015@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910016', '狮子', 'SE', 100, '10000016@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910017', '屎壳郎', 'SE', 100, '10000017@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910018', '长颈鹿', 'SE', 100, '10000018@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910019', '水牛', 'SE', 100, '10000019@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910020', '犀牛', 'SE', 100, '10000020@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910021', '豹子', 'SE', 100, '10000021@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910022', '金钱豹', 'SE', 100, '10000022@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910023', '金钱肚', 'SE', 100, '10000023@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910024', '排骨', 'SE', 100, '10000024@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910025', '牛腩', 'SE', 100, '10000025@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910026', '牛肚', 'SE', 100, '10000026@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910027', '鸭肠', 'SE', 100, '10000027@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910028', '鸭血', 'SE', 100, '10000028@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910029', '鸡翅', 'EE', 100, '10000029@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910030', '鸡腿', 'EE', 100, '10000030@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910031', '鸡翅尖', 'EE', 100, '10000031@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910032', '鱿鱼', 'EE', 100, '10000032@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910033', '鸡排', 'EE', 100, '10000033@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910034', '烤鸡', 'EE', 100, '10000034@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910035', '烤鸭', 'EE', 100, '10000035@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910036', '烤鱼', 'EE', 100, '10000036@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910037', '烤鹅', 'EE', 100, '10000037@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910038', '烧鸭', 'EE', 100, '10000038@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('319021910039', '烧鹅', 'EE', 100, '10000039@qq.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910319', '***', '信息安全', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910532', '***', 'IS', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910614', '吟啸李昂', 'IS', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910615', '***', 'IS', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910624', '越野车', 'IS', 91, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910634', '有益处', 'IS', 91, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910754', '***', '信息安全', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021910911', '以确保', 'IS', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021911109', '***', '信息安全', 99, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021911111', 'John', 'IS', 91, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021911112', '阿减了', 'IS', 91, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021911113', '时刻提防', 'IS', 91, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021911114', '大师傅', 'IS', 91, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519021911150', '***', 'IS', 98, '********@**.com');
+
+INSERT INTO `Student` (`Id`, `Name`, `Major`, `Credit`, `Email`) VALUES ('519030910224', 'XTM', 'IS', 99, '********@**.com');
+
